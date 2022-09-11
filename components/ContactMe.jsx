727d14aab53d044b9c5eb7473004b2fc4717b964
juitarist <109344723+juitarist@users.noreply.github.com>
2022-09-11 01:46:29 -0700

    Added Website Link

--- components/ContactMe.jsx
+++ components/ContactMe.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import { FaFlickr, FaInstagram, FaLinkedin, FaSoundcloud, FaTwitter } from 'react-icons/fa'
+import { FaFlickr, FaInstagram, FaLinkedin, FaSoundcloud, FaTwitter, FaSafari } from 'react-icons/fa'
 
 const ContactMe = () => {
   return (
@@ -11,14 +11,13 @@ const ContactMe = () => {
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/50 z-[2]'/>
 
 
-        <div className='max-w-[1200px] text-center justify-center items-center grid  mx-5 px-20 py-5 rounded-xl  border-1 border-red-300 bg-white/100 text-black z-[2] mt-[-10]rem
+        <div className='max-w-[1200px] text-center justify-center items-center grid  mx-5 px-20 py-5 rounded-xl  border-1 border-red-300 bg-white/100 text-black z-[2] mt-[-10]rem'>
 
-        '>
-            <h2 className='text-4xl uppercase  p-4'>Social Links</h2>
+            <h2 className='text-4xl uppercase  pt-2 pb-3 '>Social Links</h2>
             <p className='py-2 font-light text-l'>You can contact me through any of these sites!</p>
 
 
-            <div className='flex items-center justify-between max-w-[700px] m-auto py-4'> 
+            <div className='flex items-center justify-between max-w-[700px] m-auto pt-2 pb-0'> 
 
               <a href='https://linkedin.com/in/jiresare' target='_blank' className='rounded-lg shadow-lg shadow-blue-500 p-3 m-2 cursor-pointer hover:scale-150 ease-in duration-300'>
 
@@ -49,7 +48,20 @@ const ContactMe = () => {
 
             </div>
 
+            <div className='flex items-center justify-between max-w-[700px] m-auto pb-3'> 
+            
+            <a href='https://juitarist.github.io/jomariresare/' target='_blank' className='rounded-lg shadow-lg shadow-blue-500 py-3 px-36 m-2 cursor-pointer hover:scale-125 ease-in duration-300'>              
+                
+                <FaSafari size={20} />
+
+              </a>
+
+            </div>
+
+
         </div>
+
+ 
     </div>
   )
 }
