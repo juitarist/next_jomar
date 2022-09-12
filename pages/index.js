38637ae2f52e07730fc4458e5c71ec02e64fb751
juitarist <109344723+juitarist@users.noreply.github.com>
2022-09-12 02:16:10 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,27 +4,61 @@ import Hero from '../components/Hero'
 import { SliderData } from '../components/SliderData'
 import Slider from '../components/Slider'
 import Instagram from '../components/Instagram'
-import AboutMe from '../components/AboutMe'
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import Experiments from '../components/Experiments';
+import Coding from '../components/Coding';
 
 
 
-export default function Home() {
+const Home = () => {
+  const [citation, setCitation] = useState(true);
+  const [main, setMain] = useState(false);
+
+  useEffect(() => {
+    const ids = [
+      setTimeout(() => setMain(true), 100),
+    ];
+
+    return () => ids.forEach((id) => clearTimeout(id));
+  }, [setCitation]);
+
   return (
-
-
-
-    <div>
+    <>
       <Head>
         <title> jomar iresare</title>
-        <meta name="description" content="Site developed by Jomar Iresare using next.js" />
+        <meta name="description" content="Web 2.0 Homepage for Jomar Iresare, physicist and dreamer. Developed using next.js" />
         <link rel="icon" href="/favicon.ico" />
 
       </Head>
+
+      <motion.div
+        variants={{
+          initial: {
+            opacity: 0,
+            display: "none",
+          },
+          visible: {
+            opacity: 1,
+            display: "block",
+          },
+        }}
+        initial="initial"
+        animate={main ? "visible" : "initial"}
+        transition={{
+          ease: "anticipate",
+          duration: 1.0,
+        }}
+      >
       <Hero heading='jomar iresare' message='physicist.' />
       <Slider slides={SliderData} />
       <Instagram />
-    </div>
+      </motion.div>
+    </>
+  );
+};
 
 
-  )
-}
+
+
+export default Home;
